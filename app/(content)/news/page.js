0007748edcd788c7f389560da4e6df67982a3e13
devpolas <195308698+devpolas@users.noreply.github.com>
2025-08-21 23:07:18 +0600

    remove the extra backend from this project because nextjs also handel backend as well

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,14 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  // nextjs components are server side components this components not return jsx code also a async function!!
+  const news = getAllNews();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
